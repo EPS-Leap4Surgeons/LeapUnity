c020e8a17b86fe9644fa28f910b920ccbe6af835
detuur <detuur@gmail.com>
2017-04-03 19:07:53 +0200

    preliminary gesture support

---
+++ Assets/src/Test/RotationDetect.cs
@@ -0,0 +1,76 @@
+﻿using Leap.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Leap;
+using System.Linq;
+
+public class RotationDetect : MonoBehaviour {
+
+    private const int QUEUE_SAMPLES = 100;
+
+    private List<LeapQuaternion> Buffer = new List<LeapQuaternion>();
+
+    public bool Active { get; private set; }
+
+    public IHandModel HandModel;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!Active)
+            return;
+        Debug.Log("Angle is " + GetRotationAngle());
+
+	}
+
+    private string GetRotationAngle()
+    {
+        
+        Hand hand = HandModel.GetLeapHand();
+        var rot = hand.Rotation;
+        Buffer.RemoveAt(0);
+        Buffer.Add(rot);
+        rot = new LeapQuaternion(
+            Buffer.Average(elem => elem.w),
+            Buffer.Average(elem => elem.x),
+            Buffer.Average(elem => elem.y),
+            Buffer.Average(elem => elem.z));
+
+        int decimals = 4;
+
+        return Math.Round(rot.w, decimals).ToString() + " — " +
+            Math.Round(rot.x, decimals).ToString() + " — " +
+            Math.Round(rot.y, decimals).ToString() + " — " +
+            Math.Round(rot.z, decimals).ToString();
+
+        return rot.ToString();
+    }
+
+    public void TriggerGesture()
+    {
+        Active = !Active;
+        if (Active)
+        {
+            FillBuffer();
+        } else
+        {
+            Buffer.Clear();
+        }
+    }
+
+    private void FillBuffer()
+    {
+        Hand hand = HandModel.GetLeapHand();
+        var rot = hand.Rotation;
+        for (int i = 0; i < QUEUE_SAMPLES; i++)
+        {
+            Buffer.Add(rot);
+        }
+    }
+}
