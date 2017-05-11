1b9760b9ca9587fd88781a5445192377011f5c83
detuur <detuur@gmail.com>
2017-05-11 13:12:35 +0200

    Refactored classifier to proper switcher

--- Assets/src/Gestures/Classifier.cs
+++ Assets/src/Gestures/Classifier.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -9,55 +10,66 @@ using Leap.Unity;
 
 namespace Gestures
 {
-    /// <summary>
     /// The classifier was originally supposed to manage
     /// multiple gestures active in the same context.
     /// Instead, its functionality was changed to act
     /// more as a general gesture switcher and generic
     /// interface.
+    /// 
+    /// <summary>
+    /// Basically the gesture switcher.
     /// </summary>
     class Classifier : MonoBehaviour
     {
         private readonly List<GestureSequence> _gestures = new List<GestureSequence>();
+        private GestureSequence _activeGestureSequence = null;
 
+        // Use SerializeField so that we can attach the
+        // HandModel in the Unity Editor's interface
         [SerializeField] private HandModel _handModel;
 
         // Events for sequences
         [SerializeField] private RotateEvent _rotateEvent;
         [SerializeField] private PanEvent _panEvent;
 
-        bool _buttonClicked = false;
-
         public bool IsActive
         {
-            get { return _gestures.Any(g => g.State == GestureSeqState.Active); }
+            get { return _activeGestureSequence != null
+                    && _activeGestureSequence.State == GestureSeqState.Active; }
         }
-
-        // Use this for initialization
+        
         private void Start()
         {
+            _gestures.Add(new FistHoldPan(_handModel, _panEvent));
             _gestures.Add(new PinchHoldRotate(_handModel, _rotateEvent));
         }
 
+        public void ChangeTool(int tool)
+        {
+			Tool etool = (Tool)tool;
+            Type newGesture;
+            switch (etool)
+            {
+                case Tool.Panning:
+                    newGesture = typeof (FistHoldPan);
+                    break;
+                case Tool.Rotation:
+                    newGesture = typeof (PinchHoldRotate);
+                    break;
+                default:
+                    newGesture = null;
+                    break;
+            }
+			if (_activeGestureSequence.GetType() == newGesture)
+				newGesture = null;
+
+            _activeGestureSequence = _gestures.Find(g => g.GetType() == newGesture);
+        }
 
         // Update is called once per frame
         private void Update()
         {
-            if (_buttonClicked)
-            {
-                foreach (var gs in _gestures)
-                {
-                    gs.Update();
-                }
-            }     
+            _activeGestureSequence.Update();  
         }
-
-        public void ActivatePinchGesture ()
-        {
-            if (_buttonClicked) _buttonClicked = false;
-            else _buttonClicked = true;
-        }
-
-
     }
 }
